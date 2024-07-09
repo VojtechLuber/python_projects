d126eb4c75cd232a9c1efc454995b9b43a1ca0bc
VojtechLuber <vojtaluber@seznam.cz>
2024-07-09 11:38:20 +0200

    we added collumns for projects and read data in data.cvs with pandas

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -19,3 +20,20 @@ with col2:
        This experience taught me that with dedication, anything is possible.
     """
     st.info(content)
+
+content2 = """
+        lorem ipsum 
+"""
+st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
